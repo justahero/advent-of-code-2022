4be8bf1c09925c6abc33ad1b99ed73d8fff65303
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-03 12:36:20 +0100

    Implement 2nd part

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,5 +1,7 @@
 //! Day 03: Rucksack Reorganization
 
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
 /// Parses the contents of the rucksack compartments
@@ -9,44 +11,67 @@ use itertools::Itertools;
 /// 1st: 'vJrwpWtwJgWr'
 /// 2nd:             'hcsFMMfFFhFp'
 ///
-fn parse(input: &str) -> Vec<(&str, &str)> {
+fn parse(input: &str) -> Vec<&str> {
     input
         .lines()
         .map(str::trim)
         .filter(|&line| !line.is_empty())
-        .map(|line| line.split_at(line.len() / 2))
         .collect_vec()
 }
 
-/// Finds the duplicate item of both compartments
-/// Assumes that there is exactly one.
-fn find_duplicate(left: &str, right: &str) -> char {
-    let left = left.chars().collect_vec();
-    let right = right.chars().collect_vec();
-    *left.iter().find(|c| right.contains(c)).unwrap()
+fn value(c: char) -> u32 {
+    match c {
+        'a'..='z' => c as u32 - 96,
+        'A'..='Z' => c as u32 - 38,
+        _ => panic!("Unknown char found"),
+    }
 }
 
-/// Finds all duplicate items, calculates the total priority of all
-fn part1(rucksacks: &[(&str, &str)]) -> u32 {
+/// Finds all duplicate items, calculates the total priority of all.
+/// Each rucksack is split into two same size compartments.
+fn part1(rucksacks: &[&str]) -> u32 {
     rucksacks
         .iter()
-        .map(|(l, r)| find_duplicate(l, r))
-        .map(|item| match item {
-            'a'..='z' => item as u32 - 96,
-            'A'..='Z' => item as u32 - 38,
-            _ => panic!("Unknown char found"),
+        .map(|line| line.split_at(line.len() / 2))
+        .map(|(left, right)| {
+            let left = left.chars().collect::<HashSet<_>>();
+            let right = right.chars().collect::<HashSet<_>>();
+            left.intersection(&right).next().unwrap().clone()
         })
+        .map(value)
+        .sum()
+}
+
+/// Find the badge in each group of elves. Each group consists of three elves. All three
+/// elves' rucksack contain the same badge.
+fn part2(rucksacks: &[&str]) -> u32 {
+    rucksacks
+        .iter()
+        .map(|&s| s.chars().collect::<HashSet<_>>())
+        .chunks(3)
+        .into_iter()
+        .map(|group| {
+            group
+                .reduce(|result: HashSet<char>, rhs| result.intersection(&rhs).cloned().collect())
+                .unwrap()
+                .iter()
+                .next()
+                .unwrap()
+                .clone()
+        })
+        .map(value)
         .sum()
 }
 
 fn main() {
     let input = parse(include_str!("input.txt"));
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse, part1};
+    use crate::{parse, part1, part2};
 
     const INPUT: &str = r#"
         vJrwpWtwJgWrhcsFMMfFFhFp
@@ -63,5 +88,7 @@ mod tests {
     }
 
     #[test]
-    fn check_part2() {}
+    fn check_part2() {
+        assert_eq!(70, part2(&parse(INPUT)));
+    }
 }
