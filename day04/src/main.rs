15cc2bd48a28f15fe7ecb4442b8544b1735273c6
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-04 22:27:22 +0100

    Finish 2nd part of day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -12,8 +12,12 @@ impl Assignment {
         Self { l, r }
     }
 
-    fn overlaps(&self) -> bool {
+    fn covers(&self) -> bool {
         self.l.contains(&self.r) || self.r.contains(&self.l)
+    }
+
+    fn intersects(&self) -> bool {
+        self.l.intersect(&self.r)
     }
 }
 
@@ -29,8 +33,16 @@ impl Range {
         Self { min, max }
     }
 
+    /// Returns `true` if the given [`Range`] is completely contained
     pub fn contains(&self, rhs: &Self) -> bool {
         self.min <= rhs.min && self.max >= rhs.max
+    }
+
+    /// Returns `true` if both ranges intersect with each other.
+    ///
+    /// Both sections intersect when any bound is inside the bounds of the other.
+    pub fn intersect(&self, rhs: &Self) -> bool {
+        self.max >= rhs.min && rhs.max >= self.min
     }
 }
 
@@ -43,10 +55,19 @@ impl From<&str> for Range {
     }
 }
 
+/// Returns the total number of overlapping assignments, where one part overlaps completely
 fn part1(sections: &[Assignment]) -> usize {
     sections
         .iter()
-        .filter(|&assignment| assignment.overlaps())
+        .filter(|&assignment| assignment.covers())
+        .count()
+}
+
+/// Returns the total number of any overlapping assignments
+fn part2(sections: &[Assignment]) -> usize {
+    sections
+        .iter()
+        .filter(|&assignment| assignment.intersects())
         .count()
 }
 
@@ -56,14 +77,14 @@ fn parse(input: &str) -> Vec<Assignment> {
         .map(str::trim)
         .filter(|s| !s.is_empty())
         .filter_map(|s| s.split_once(','))
-        .map(|(l, r)| (l.into(), r.into()))
-        .map(|(l, r)| Assignment::new(l, r))
+        .map(|(l, r)| Assignment::new(l.into(), r.into()))
         .collect::<Vec<_>>()
 }
 
 fn main() {
     let input = parse(include_str!("input.txt"));
     println!("Overlaps: {}", part1(&input));
+    println!("Intersections: {}", part2(&input));
 }
 
 #[cfg(test)]
@@ -80,14 +101,28 @@ mod tests {
     "#;
 
     #[test]
-    fn check_flat_map() {
-        let values = [true, true, false, false, false];
-        let count = values.iter().filter_map(|v| v.into()).count();
-        println!("Count: {}", count);
+    fn check_range_bounds() {
+        let l = Range::new(2, 8);
+        let r = Range::new(3, 7);
+        assert!(l.contains(&r));
+        assert!(!r.contains(&l));
+    }
+
+    #[test]
+    fn check_range_intersects() {
+        let l = Range::new(5, 7);
+        let r = Range::new(7, 9);
+        assert!(l.intersect(&r));
+        assert!(r.intersect(&l));
     }
 
     #[test]
     fn check_part1() {
         assert_eq!(2, part1(&parse(INPUT)));
+    }
+
+    #[test]
+    fn check_part2() {
+        assert_eq!(4, part2(&parse(INPUT)));
     }
 }
