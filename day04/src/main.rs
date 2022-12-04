b547f0073a35cf4a6e0032df42e0e64e76a0412d
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-04 17:38:43 +0100

    Implement first part

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,23 +1,69 @@
 //! Day 04: Camp Cleanup
-use bitvec::prelude::*;
 
-fn part1(sections: &[BitVec]) -> u32 {
-    todo!()
+struct Assignment {
+    /// First or left sections
+    pub l: Range,
+    /// Second or right sections
+    pub r: Range,
 }
 
-fn parse(input: &str) -> Vec<BitVec> {
-    let x = input
+impl Assignment {
+    pub fn new(l: Range, r: Range) -> Self {
+        Self { l, r }
+    }
+
+    fn overlaps(&self) -> bool {
+        self.l.contains(&self.r) || self.r.contains(&self.l)
+    }
+}
+
+struct Range {
+    /// The lower or minimum bound
+    min: u32,
+    /// The upper or maximum bound
+    max: u32,
+}
+
+impl Range {
+    pub fn new(min: u32, max: u32) -> Self {
+        Self { min, max }
+    }
+
+    pub fn contains(&self, rhs: &Self) -> bool {
+        self.min <= rhs.min && self.max >= rhs.max
+    }
+}
+
+impl From<&str> for Range {
+    fn from(range: &str) -> Self {
+        let (min, max) = range.split_once('-').expect("Failed to parse range.");
+        let min = min.parse::<u32>().expect("Not a number");
+        let max = max.parse::<u32>().expect("Not a number");
+        Range::new(min, max)
+    }
+}
+
+fn part1(sections: &[Assignment]) -> usize {
+    sections
+        .iter()
+        .filter(|&assignment| assignment.overlaps())
+        .count()
+}
+
+fn parse(input: &str) -> Vec<Assignment> {
+    input
         .lines()
         .map(str::trim)
         .filter(|s| !s.is_empty())
-        .filter_map(|s| s.split_once(","))
-        .collect::<Vec<_>>();
-
-    Vec::new()
+        .filter_map(|s| s.split_once(','))
+        .map(|(l, r)| (l.into(), r.into()))
+        .map(|(l, r)| Assignment::new(l, r))
+        .collect::<Vec<_>>()
 }
 
 fn main() {
     let input = parse(include_str!("input.txt"));
+    println!("Overlaps: {}", part1(&input));
 }
 
 #[cfg(test)]
@@ -32,6 +78,13 @@ mod tests {
         6-6,4-6
         2-6,4-8
     "#;
+
+    #[test]
+    fn check_flat_map() {
+        let values = [true, true, false, false, false];
+        let count = values.iter().filter_map(|v| v.into()).count();
+        println!("Count: {}", count);
+    }
 
     #[test]
     fn check_part1() {
