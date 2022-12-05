7a88910eb575cdb8dac61d7c3cf948aa145e0ebf
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-05 20:56:27 +0100

    Add day 5 scaffold

---
+++ day05/src/main.rs
@@ -0,0 +1,27 @@
+//! Day 05: Supply Stacks
+
+fn part1() {
+    todo!()
+}
+
+fn parse(input: &str) -> () {
+    todo!("")
+}
+
+fn main() {
+    let input = parse(include_str!("input.txt"));
+    println!("Part 1: {}", part1(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    const INPUT: &str = r#"
+    "#;
+
+    #[test]
+    fn check_part1() {
+        assert_eq!(2, part1(&parse(INPUT)));
+    }
+}
