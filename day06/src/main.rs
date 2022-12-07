4ecd1d1311e0e8ec8ded74aed9f7bebee02dadf1
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-07 23:56:43 +0100

    Adjust day 06 solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,38 +1,31 @@
 //! Day 06: Tuning Trouble
 
-use std::collections::HashSet;
-
 use itertools::Itertools;
+
+use std::collections::HashSet;
 
 /// Detects the start of packet marker, a four letter sequence where all letters are different
 ///
 /// The first position of such a sequence is returned.
 fn marker_pos(datastream: &str, distinct: usize) -> Option<usize> {
-    let datastream = datastream.chars().collect_vec();
-    for index in 0..(datastream.len() - distinct) {
-        let chars = datastream[index..index + distinct].iter().collect::<HashSet<_>>();
-        if chars.len() == distinct {
-            return Some(index + distinct);
-        }
-    }
-
-    None
+    let chars = datastream.chars().collect::<Vec<_>>();
+    chars
+        .windows(distinct)
+        .map(|seq| seq.iter().collect::<HashSet<_>>())
+        .position(|seq| seq.len() == distinct)
+        .map(|pos| pos + distinct)
 }
 
-fn parse(input: &str) -> Vec<&str> {
-    input.lines().map(str::trim).collect::<Vec<_>>()
+fn part1(line: &str) -> usize {
+    marker_pos(line, 4).expect("Failed to find start of packet")
 }
 
-fn part1(lines: &[&str]) -> usize {
-    lines.iter().filter_map(|line| marker_pos(line, 4)).sum()
-}
-
-fn part2(lines: &[&str]) -> usize {
-    lines.iter().filter_map(|line| marker_pos(line, 14)).sum()
+fn part2(line: &str) -> usize {
+    marker_pos(line, 14).expect("Failed to find start of packet")
 }
 
 fn main() {
-    let lines = parse(include_str!("input.txt"));
+    let lines = include_str!("input.txt");
     println!("Part 1: {}", part1(&lines));
     println!("Part 2: {}", part2(&lines));
 }
@@ -42,20 +35,31 @@ mod tests {
     use crate::*;
 
     #[test]
+    fn test_tuple_windows() {
+        let slice = [1, 2, 3, 4, 5];
+
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        for (index, (c1, c2, c3, c4)) in input.chars().tuple_windows().enumerate() {}
+
+        let chars = input.chars().collect_vec();
+        // chars.windows(size)
+    }
+
+    #[test]
     fn check_part1() {
-        assert_eq!(7, part1(&["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]));
-        assert_eq!(5, part1(&["bvwbjplbgvbhsrlpgdmjqwftvncz"]));
-        assert_eq!(6, part1(&["nppdvjthqldpwncqszvftbrmjlhg"]));
-        assert_eq!(10, part1(&["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"]));
-        assert_eq!(11, part1(&["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"]));
+        assert_eq!(7, part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!(5, part1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(6, part1("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(10, part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(11, part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
     }
 
     #[test]
     fn check_part2() {
-        assert_eq!(19, part2(&["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]));
-        assert_eq!(23, part2(&["bvwbjplbgvbhsrlpgdmjqwftvncz"]));
-        assert_eq!(23, part2(&["nppdvjthqldpwncqszvftbrmjlhg"]));
-        assert_eq!(29, part2(&["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"]));
-        assert_eq!(26, part2(&["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"]));
+        assert_eq!(19, part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assert_eq!(23, part2("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assert_eq!(23, part2("nppdvjthqldpwncqszvftbrmjlhg"));
+        assert_eq!(29, part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assert_eq!(26, part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
     }
 }
