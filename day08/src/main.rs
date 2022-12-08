e0085a4920e2aa645bc3baa76d5ef5815cd6fbf9
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-08 21:33:59 +0100

    Use a steps iterator using Pos

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -21,6 +21,54 @@ impl Display for TreeGrid {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Pos {
+    x: i32,
+    y: i32,
+}
+
+impl Pos {
+    pub const fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl std::ops::Add for Pos {
+    type Output = Pos;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Pos::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+struct Steps<'a> {
+    grid: &'a TreeGrid,
+    start: Pos,
+    dir: Pos,
+}
+
+impl<'a> Steps<'a> {
+    pub fn new(grid: &'a TreeGrid, start: Pos, dir: Pos) -> Self {
+        Self { grid, start, dir }
+    }
+}
+
+impl<'a> Iterator for Steps<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let start = self.start + self.dir;
+
+        match self.grid.get(start) {
+            Some(value) => {
+                self.start = start;
+                Some(value)
+            }
+            None => None
+        }
+    }
+}
+
 impl TreeGrid {
     pub fn new() -> Self {
         Self {
@@ -30,7 +78,7 @@ impl TreeGrid {
         }
     }
 
-    /// Adds a tree line to the grid
+    /// Adds a row of trees to the grid
     pub fn add_line(mut self, mut line: Vec<u8>) -> Self {
         self.width = line.len();
         self.height += 1;
@@ -45,29 +93,27 @@ impl TreeGrid {
 
     pub fn invisibles(&self) -> u64 {
         let mut invisibles = 0;
-        let directions: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+        const DIRECTIONS: [Pos; 4] = [Pos::new(1, 0), Pos::new(0, 1), Pos::new(-1, 0), Pos::new(0, -1)];
+
         for y in 1..(self.height - 1) as i32 {
             for x in 1..(self.width - 1) as i32 {
-                let tree = self.get(x, y).expect(format!("No tree found at {}x{}?", x, y).as_str());
+                let tree = self.get(Pos::new(x, y)).expect(format!("No tree found at {}x{}?", x, y).as_str());
 
-                // check all directions
-                let (mut dx, mut dy) = (x, y);
-                'outer: for dir in directions {
-                    while let Some(neighbor) = self.get(dx + dir.0, dy + dir.1) {
-                        if tree < neighbor {
-                            invisibles += 1;
-                            break 'outer;
-                        }
-                        dx += dir.0;
-                        dy += dir.1;
-                    }
+                // A tree is invisible when it cannot be seen from any direction
+                let invisible = DIRECTIONS.iter().all(|&dir| self.steps(Pos::new(x, y), dir).any(|neighbor| tree <= neighbor));
+                if invisible {
+                    invisibles += 1;
                 }
             }
         }
         invisibles
     }
 
-    fn get(&self, x: i32, y: i32) -> Option<u8> {
+    fn steps(&self, pos: Pos, dir: Pos) -> Steps {
+        Steps::new(self, pos, dir)
+    }
+
+    fn get(&self, Pos{ x, y }: Pos) -> Option<u8> {
         if 0 <= x && x < self.width as i32 && 0 <= y && y < self.height as i32 {
             Some(self.trees[(y * self.width as i32 + x) as usize])
         } else {
