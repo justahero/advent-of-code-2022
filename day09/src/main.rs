62be1d1d33cb13a357c481016fac81e2cbde6cd7
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-09 09:57:41 +0100

    Implement solution for part 1

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,8 +1,11 @@
 //! Day 08: Rope Bridge
 
-#[derive(Debug)]
+use std::collections::HashSet;
+
+#[derive(Debug, Copy, Clone)]
+#[repr(u8)]
 enum Direction {
-    Right,
+    Right = 0,
     Up,
     Left,
     Down,
@@ -20,7 +23,7 @@ impl From<&str> for Direction {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Pos {
     x: i32,
     y: i32,
@@ -30,27 +33,25 @@ impl Pos {
     pub const fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
-}
 
-#[derive(Debug)]
-struct Grid {
-    head: Pos,
-    tail: Pos,
-    trail: Vec<Pos>,
-}
-
-impl Grid {
-    pub fn new() -> Self {
-        Self {
-            head: Pos::new(0, 0),
-            tail: Pos::new(0, 0),
-            trail: Vec::new(),
-        }
+    /// Checks if this pos is adjacent / touching the other
+    pub fn touches(&self, rhs: &Pos) -> bool {
+        (self.x - rhs.x).abs() <= 1 && (self.y - rhs.y).abs() <= 1
     }
+}
 
-    /// Apply the given step, move head and tail, keep track of where tail stepped.
-    pub fn step(&mut self, m: &Move) {
-        todo!()
+impl std::ops::Add for Pos {
+    type Output = Pos;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Pos::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl std::ops::AddAssign for Pos {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
     }
 }
 
@@ -66,12 +67,79 @@ impl Move {
     }
 }
 
-impl std::ops::Add for Pos {
-    type Output = Pos;
+#[derive(Debug)]
+struct Grid {
+    /// The head position
+    head: Pos,
+    /// Keeps the position where head touched tail the last time
+    last_head: Pos,
+    /// The tail position
+    tail: Pos,
+    /// The list of all visited position by tail
+    trail: HashSet<Pos>,
+}
 
-    fn add(self, rhs: Self) -> Self::Output {
-        Pos::new(self.x + rhs.x, self.y + rhs.y)
+impl Grid {
+    const DIRECTIONS: [Pos; 4] = [
+        Pos::new(1, 0),  // right
+        Pos::new(0, -1), // up
+        Pos::new(-1, 0), // left
+        Pos::new(0, 1),  // down
+    ];
+
+    pub fn new() -> Self {
+        let trail = HashSet::from([Pos::new(0, 0)]);
+        Self {
+            head: Pos::new(0, 0),
+            last_head: Pos::new(0, 0),
+            tail: Pos::new(0, 0),
+            trail,
+        }
     }
+
+    /// Apply the given step, move head and tail, keep track of where tail stepped.
+    pub fn step(&mut self, m: &Move) {
+        for _ in 0..m.steps {
+            let dir = Self::DIRECTIONS[m.dir as usize];
+            self.last_head = self.head;
+            self.head += dir;
+
+            // update tail position if head moves away
+            if !self.tail.touches(&self.head) {
+                self.tail = self.last_head;
+                self.trail.insert(self.tail);
+            }
+
+            // self.print_trail();
+        }
+    }
+
+    /*
+    pub fn print_trail(&self) {
+        let (minx, maxx) = (-2, 7);
+        let (miny, maxy) = (-6, 3);
+
+        for y in miny..=maxy {
+            for x in minx..=maxx {
+                let pos = Pos::new(x, y);
+                if self.tail == pos {
+                    if self.head != self.tail {
+                        print!("T");
+                    }
+                } else if self.head == pos {
+                    print!("H");
+                } else {
+                    match self.trail.get(&pos) {
+                        Some(_) => print!("#"),
+                        None => print!("."),
+                    }
+                }
+            }
+            println!();
+        }
+        println!();
+    }
+    */
 }
 
 fn parse(input: &str) -> Vec<Move> {
@@ -95,12 +163,15 @@ fn part1(moves: &[Move]) -> usize {
     for m in moves {
         grid.step(m);
     }
+    // grid.print_trail();
     grid.trail.len()
 }
 
 fn main() {
-    let grid = parse(include_str!("input.txt"));
-    println!("Part 1: {}", part1(&grid));
+    let moves = parse(include_str!("input.txt"));
+    let count = part1(&moves);
+    assert!(count > 6262);
+    println!("Part 1: {}", part1(&moves));
 }
 
 #[cfg(test)]
