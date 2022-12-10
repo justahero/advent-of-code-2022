106ff5111845f0034f0d20faf4ee64e8bba53426
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-10 12:53:43 +0100

    Implement solution for first part

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -16,12 +16,9 @@ fn parse_number(input: &str) -> IResult<&str, i32> {
 }
 
 #[derive(Debug)]
-struct Add(i32);
-
-#[derive(Debug)]
 enum Instruction {
     Noop,
-    Add(Add),
+    Add(i32),
 }
 
 impl Instruction {
@@ -35,7 +32,7 @@ impl Instruction {
     pub fn parse(input: &str) -> IResult<&str, Self> {
         alt((
             map(tag("noop"), |_| Instruction::Noop),
-            map(parse_add, |n| Instruction::Add(Add(n))),
+            map(parse_add, |n| Instruction::Add(n)),
         ))(input)
     }
 }
@@ -43,25 +40,53 @@ impl Instruction {
 #[derive(Debug)]
 struct VideoSystem {
     /// The start cycle counter that is increased for each instructions
-    cycle_count: i32,
+    start_interval: u32,
     /// The cycle interval to measure signal strength of 'X'
     interval: u32,
     /// The accumulated register 'X'
-    x: i32,
+    x: i64,
 }
 
 impl VideoSystem {
-    pub fn new(start_count: i32, interval: u32) -> Self {
+    pub fn new(start_interval: u32, interval: u32) -> Self {
         Self {
-            cycle_count: start_count,
+            start_interval,
             interval,
             x: 1,
         }
     }
 
     /// Run all given instructions on the video system, take care of cycles and measure X at certain intervals.
-    pub fn run(&self, instructions: &[Instruction]) -> u64 {
-        0
+    pub fn run(&self, instructions: &[Instruction]) -> Vec<(i64, i64)> {
+        let mut signal_strengths = Vec::new();
+        let mut cycle_count = 0;
+        let mut interval = self.start_interval;
+        let mut x = self.x;
+
+        for instruction in instructions {
+            // run all cycles
+            for _ in 0..instruction.cycles() {
+                cycle_count += 1;
+                interval += 1;
+
+                // in case an interval is hit store current signal / cycle pair
+                if interval >= self.interval {
+                    println!(
+                        "INTERVAL: {}, cycle count: {}, x: {}",
+                        interval, cycle_count, x
+                    );
+                    interval = 0;
+                    signal_strengths.push((cycle_count as i64, x as i64));
+                }
+            }
+            // apply operation
+            if let Instruction::Add(value) = instruction {
+                println!("ADD {}", value);
+                x += *value as i64;
+            }
+        }
+
+        signal_strengths
     }
 }
 
@@ -71,9 +96,15 @@ fn parse(input: &str) -> anyhow::Result<Vec<Instruction>> {
     Ok(instructions)
 }
 
-fn part1(instructions: &[Instruction]) -> u64 {
-    let video_system = VideoSystem::new(-20, 40);
-    video_system.run(instructions)
+fn part1(instructions: &[Instruction]) -> i64 {
+    let video_system = VideoSystem::new(20, 40);
+    let strengths = video_system.run(instructions);
+
+    println!("STRENGTHS: {:?}", strengths);
+    strengths
+        .iter()
+        .map(|(cycle, value)| *cycle * *value)
+        .sum::<i64>()
 }
 
 fn main() -> anyhow::Result<()> {
@@ -91,6 +122,20 @@ mod tests {
     #[test]
     fn check_part1() {
         let instructions = parse(INPUT).expect("Failed to parse input.");
+        let system = VideoSystem::new(20, 40);
+        let signal_strengths = system.run(&instructions);
+
+        assert_eq!(
+            vec![
+                (20, 21),
+                (60, 19),
+                (100, 18),
+                (140, 21),
+                (180, 16),
+                (220, 18)
+            ],
+            signal_strengths
+        );
         assert_eq!(13140, part1(&instructions));
     }
 }
