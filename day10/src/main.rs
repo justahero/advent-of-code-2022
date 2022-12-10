28688df234a2f2f459067afee72d1acd52e93deb
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-10 12:11:11 +0100

    Use nom parser to read input

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,10 +1,63 @@
-//! Day 10:
+//! Day 10: Cathode-Ray Tube
 
-fn parse(input: &str) {
-    input
-        .lines()
-        .map(str::trim)
-        .filter(|l| !l.is_empty());
+use nom::{
+    branch::alt, bytes::complete::tag, character::complete::newline, combinator::map,
+    multi::separated_list1, sequence::preceded, IResult,
+};
+
+fn parse_add(input: &str) -> IResult<&str, i32> {
+    preceded(tag("addx "), parse_number)(input)
+}
+
+fn parse_number(input: &str) -> IResult<&str, i32> {
+    nom::character::complete::i32(input)
+}
+
+#[derive(Debug)]
+struct Add(i32);
+
+#[derive(Debug)]
+enum Instruction {
+    Noop,
+    Add(Add),
+}
+
+impl Instruction {
+    pub fn cycles(&self) -> u32 {
+        match self {
+            Instruction::Noop => 1,
+            Instruction::Add(_) => 2,
+        }
+    }
+
+    pub fn parse(input: &str) -> IResult<&str, Self> {
+        alt((
+            map(tag("noop"), |_| Instruction::Noop),
+            map(parse_add, |n| Instruction::Add(Add(n))),
+        ))(input)
+    }
+}
+
+#[derive(Debug)]
+struct VideoSystem {
+    /// The number of cycles
+    cycle_count: u32,
+    /// The accumulated register 'X'
+    x: i32,
+}
+
+impl VideoSystem {
+    pub fn new() -> Self {
+        Self {
+            cycle_count: 0,
+            x: 1,
+        }
+    }
+}
+
+fn parse(input: &str) -> anyhow::Result<Vec<Instruction>> {
+    let (_, instructions) = separated_list1(newline, Instruction::parse)(input).unwrap();
+    Ok(instructions)
 }
 
 fn part1() -> usize {
@@ -20,8 +73,7 @@ fn main() {
 mod tests {
     use crate::*;
 
-    const INPUT: &str = r#"
-    "#;
+    const INPUT: &str = include_str!("test.txt");
 
     #[test]
     fn check_part1() {
