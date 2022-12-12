3bcdf57a163aa6b2cf00871e2a20a3fd2ad7636e
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-12 21:19:59 +0100

    Trying to find solution using breadth first search

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,15 +1,42 @@
 //! Day 12: Hill Climbing Algorithm
 
+use std::{
+    collections::VecDeque,
+    fmt::{Display, Formatter},
+};
+
 use itertools::Itertools;
 
+#[derive(Debug, Hash, PartialEq, Eq, Copy, Clone)]
 struct Pos {
     x: i32,
     y: i32,
 }
 
 impl Pos {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub const fn new(x: i32, y: i32) -> Self {
         Self { x, y }
+    }
+}
+
+impl std::ops::Add for Pos {
+    type Output = Pos;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Pos::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl std::ops::AddAssign for Pos {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl Display for Pos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)
     }
 }
 
@@ -22,6 +49,13 @@ struct Grid {
 }
 
 impl Grid {
+    const DIRECTIONS: [Pos; 4] = [
+        Pos::new(1, 0),  // right
+        Pos::new(0, 1),  // down
+        Pos::new(-1, 0), // left
+        Pos::new(0, -1), // up
+    ];
+
     pub fn new() -> Self {
         Self {
             cells: Vec::new(),
@@ -41,21 +75,86 @@ impl Grid {
         if let Some(x) = row.iter().position(|&c| c == 'E') {
             self.end = Pos::new(x as i32, self.height as i32);
         }
+
         self.cells.append(&mut row);
+        self.height += 1;
         self
     }
 
-    pub fn traverse_steps(&self) -> usize {
-        0
+    /// Breadth depth search
+    fn find_path(&self) -> usize {
+        let mut visited: Vec<Pos> = vec![self.start];
+        let mut positions: VecDeque<Pos> = VecDeque::new();
+
+        positions.push_back(self.start);
+        visited.push(self.start);
+
+        while let Some(pos) = positions.pop_front() {
+            let current = self.get(pos).expect("Failed to get height.");
+            println!("VISITED: {:?}", visited);
+
+            if current == 'E' {
+                break;
+            }
+
+            for neighbor in self.neighbors(pos).into_iter() {
+                if visited.contains(&neighbor) {
+                    continue;
+                }
+
+                if let Some(elevation) = self.get(neighbor) {
+                    // can only pass one step up but lower is always possible
+                    if current as u8 + 1 <= elevation as u8 {
+                        println!("  N: {} - {}", neighbor, elevation as char);
+                        positions.push_back(neighbor);
+                        visited.push(neighbor);
+                    }
+                }
+            }
+        }
+
+        visited.len()
     }
 
-    fn neighbors(&self, pos: &Pos) -> impl Iterator<Item = &char> + '_ {
-        self.cells.iter()
+    fn neighbors(&self, pos: Pos) -> Vec<Pos> {
+        let mut neighbors = Vec::new();
+        for &dir in Self::DIRECTIONS.iter() {
+            let neighbor = pos + dir;
+            if self.is_inside(neighbor) {
+                neighbors.push(neighbor);
+            }
+        }
+        neighbors
+    }
+
+    fn is_inside(&self, Pos { x, y }: Pos) -> bool {
+        0 <= x && x < self.width as i32 && 0 <= y && y < self.height as i32
+    }
+
+    fn get(&self, Pos { x, y }: Pos) -> Option<char> {
+        if 0 <= x && x < self.width as i32 && 0 <= y && y < self.height as i32 {
+            Some(self.cells[(y * self.width as i32 + x) as usize])
+        } else {
+            None
+        }
+    }
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for row in self.cells.chunks(self.width) {
+            for c in row {
+                write!(f, "{}", *c as char)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
 
 fn part1(grid: &Grid) -> usize {
-    grid.traverse_steps()
+    println!("{}", grid);
+    grid.find_path()
 }
 
 fn parse(input: &str) -> Grid {
@@ -68,7 +167,10 @@ fn parse(input: &str) -> Grid {
     grid
 }
 
-fn main() {}
+fn main() {
+    let grid = parse(include_str!("input.txt"));
+    println!("Part 1: {}", part1(&grid));
+}
 
 #[cfg(test)]
 mod tests {
