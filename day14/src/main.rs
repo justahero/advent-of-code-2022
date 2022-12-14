204c9e3024fd12b30c77025c30cd9a741b39dd6a
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-14 21:45:41 +0100

    Parse input into points & lines

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,31 +1,59 @@
 //! Day 14: Regolith Reservoir
 
+use nom::{
+    bytes::complete::tag,
+    character::complete::digit1,
+    combinator::{map, map_res},
+    multi::separated_list1,
+    sequence::tuple,
+    IResult,
+};
+
 struct Rect {
     min: Pos,
     max: Pos,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Pos {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug)]
-struct Line {
-    start: Pos,
-    end: Pos,
+impl Pos {
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
 }
 
-fn parse_lines(input: &str) -> Vec<Line> {
+#[derive(Debug, Default, PartialEq, Eq)]
+struct PolyLine {
+    points: Vec<Pos>,
+}
 
+impl PolyLine {
+    pub fn new(points: Vec<Pos>) -> Self {
+        Self { points }
+    }
+}
 
-    Vec::new()
+fn parse_pair(input: &str) -> IResult<&str, Pos> {
+    let (input, (x, _, y)) = tuple((
+        map_res(digit1, str::parse::<i32>),
+        tag(","),
+        map_res(digit1, str::parse::<i32>),
+    ))(input)?;
+    Ok((input, Pos::new(x, y)))
+}
+
+fn parse_lines(input: &str) -> PolyLine {
+    let (_, points) = separated_list1(tag(" -> "), parse_pair)(input).unwrap();
+    PolyLine::new(points)
 }
 
 #[derive(Debug)]
 struct Grid {
-    lines: Vec<Line>,
+    lines: Vec<PolyLine>,
 }
 
 impl Grid {
@@ -43,8 +71,8 @@ fn parse(input: &str) -> Grid {
         .lines()
         .map(str::trim)
         .filter(|line| !line.is_empty())
-        .map(|line| parse_lines(input))
-        .collect::<Vec<Vec<Line>>>();
+        .map(parse_lines)
+        .collect::<Vec<PolyLine>>();
 
     Grid::new()
 }
@@ -62,6 +90,17 @@ mod tests {
         498,4 -> 498,6 -> 496,6
         503,4 -> 502,4 -> 502,9 -> 494,9
     "#;
+
+    #[test]
+    fn check_parse_lines() {
+        assert_eq!(Pos::new(498, 4), parse_pair("498,4").unwrap().1);
+        assert_eq!(
+            PolyLine {
+                points: vec![Pos::new(498, 4), Pos::new(498, 6), Pos::new(496, 6)]
+            },
+            parse_lines("498,4 -> 498,6 -> 496,6")
+        );
+    }
 
     #[test]
     fn check_part1() {
