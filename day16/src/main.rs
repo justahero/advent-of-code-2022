5fb62fa5c66a902a377fa6a81d5f55112d399dcb
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-16 23:55:30 +0100

    Trying to fix first solution

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -7,8 +7,8 @@ peg::parser! {
         rule valve() -> String
             = s:$(['A'..='Z']+) { s.to_string() }
 
-        rule rate() -> u32
-            = n:$(['0'..='9']+) {? n.parse::<u32>().or(Err("Failed to parse number")) }
+        rule rate() -> i32
+            = n:$(['0'..='9']+) {? n.parse::<i32>().or(Err("Failed to parse number")) }
 
         rule tunnels() -> Vec<String>
             = valves:(valve() ** ", ") { valves }
@@ -35,51 +35,87 @@ struct Valve(String);
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Pipe {
     valve: String,
-    flow_rate: u32,
+    flow_rate: i32,
     tunnels: Vec<String>,
     open: bool,
 }
 
 /// Determine the best path to maximume the flow of all open valves.
-fn open_valves(mut pipes: Vec<Pipe>, num_rounds: u32) -> usize {
+fn open_valves(pipes: Vec<Pipe>, num_rounds: i32) -> usize {
     // Look up table
-    let pipes_by_label: BTreeMap<String, Pipe> = pipes
-        .iter()
-        .map(|pipe| (pipe.valve.to_string(), pipe.clone()))
+    let mut pipes_by_label: BTreeMap<String, Pipe> = pipes
+        .into_iter()
+        .map(|pipe| (pipe.valve.to_string(), pipe))
         .collect::<BTreeMap<_, _>>();
 
     let mut current_valve = "AA".to_string();
+    let mut current_steps = 0;
+    let mut total_flow: i32 = 0;
 
     // play all rounds
-    for round in (0..1).rev() {
+    let mut round = 0;
+    while round < num_rounds {
         println!("ROUND: {}", round);
-        
-        // find all possible valves
-        let all_paths = dijkstra_all(&current_valve, |valve| {
-            let mut valves: Vec<(String, u32)> = Vec::new();
-            let current_pipe = pipes_by_label.get(valve).expect("Failed to get pipe");
 
-            for tunnel in &current_pipe.tunnels {
-                let pipe = pipes_by_label.get(tunnel).expect("Failed to get successor");
-                valves.push((tunnel.to_string(), 1));
+        // calculcate flow since last step
+        total_flow += pipes_by_label
+            .values()
+            .filter(|pipe| pipe.open)
+            .inspect(|pipe| println!("PIPE: {:?}", pipe))
+            .map(|pipe| pipe.flow_rate)
+            .inspect(|flow| println!("FLOW: {}", flow))
+            .sum::<i32>();
+
+        println!("TOTAL: {}", total_flow);
+
+        current_steps -= 1;
+
+        // TODO update the pipe!!!
+        pipes_by_label.get_mut(&current_valve).unwrap().open = true;
+
+        // currently on the way to new valve
+        if current_steps == 0 {
+            // evaluate current network of open valves
+            let all_paths = dijkstra_all(&current_valve.clone(), |valve| {
+                let mut valves: Vec<(String, i32)> = Vec::new();
+                let current_pipe = pipes_by_label.get(valve).expect("Failed to get pipe");
+
+                for tunnel in &current_pipe.tunnels {
+                    let pipe = pipes_by_label.get(tunnel).expect("Failed to get successor");
+                    valves.push((pipe.valve.to_string(), 1));
+                }
+
+                valves
+            });
+
+            // find the best possible candidate that provides the highest flow
+            let next_valve = all_paths
+                .iter()
+                .filter_map(|(key, (_valve, steps))| {
+                    let pipe = pipes_by_label.get(key).unwrap();
+                    if !pipe.open {
+                        let flow_rate = (num_rounds - round - steps).max(0) * pipe.flow_rate;
+                        Some((key, flow_rate, steps))
+                    } else {
+                        None
+                    }
+                })
+                .max_by(|left, right| left.1.cmp(&right.1));
+
+            // when there is still one valve left, advance
+            if let Some((next_valve, _, steps)) = next_valve {
+                current_valve = next_valve.clone();
+                current_steps = *steps;
             }
-
-            valves
-        });
-
-
-        // let paths = all_paths.get(&current_valve).unwrap();
-        for path in all_paths.iter() {
-            println!("  PATH: {:?}", path);
         }
 
-        // println!("{:?}", all_paths);
+        round += 1;
     }
 
-    0
+    total_flow as usize
 }
 
-fn part1(mut pipes: Vec<Pipe>) -> usize {
+fn part1(pipes: Vec<Pipe>) -> usize {
     open_valves(pipes, 30)
 }
 
@@ -126,7 +162,7 @@ mod tests {
             }),
             line_parser::pipe("Valve AA has flow rate=0; tunnels lead to valves DD, II, BB"),
         );
-        
+
         // one entry with 'tunnel' instead of 'tunnels'
         assert!(line_parser::pipe("Valve HH has flow rate=22; tunnel leads to valve GG").is_ok());
     }
