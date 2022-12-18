6a6cc7eecf1dc35d26f83c15a5ee74cad35c76a5
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-18 22:01:38 +0100

    Implement solution for part 2
    * add a lot of helper methods
    * use floodfill algorithm to find exposed cubes

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,24 +1,78 @@
 //! Day 18: Boiling Boulders
 
-use std::ops::Index;
+use std::{collections::HashSet, ops::Index};
 
 use anyhow::anyhow;
 
 use itertools::Itertools;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct Cube([i32; 3]);
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+struct Vec3([i32; 3]);
 
-impl Cube {
-    pub fn neighbor(&self, rhs: &Cube) -> bool {
-        let a = self[0] == rhs[0] && self[1] == rhs[1] && (self[2] - rhs[2]).abs() == 1;
-        let b = self[1] == rhs[1] && self[2] == rhs[2] && (self[0] - rhs[0]).abs() == 1;
-        let c = self[2] == rhs[2] && self[0] == rhs[0] && (self[1] - rhs[1]).abs() == 1;
-        a || b || c
+impl Default for Vec3 {
+    fn default() -> Self {
+        Self([0, 0, 0])
     }
 }
 
-impl Index<usize> for Cube {
+impl Vec3 {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
+        Self([x, y, z])
+    }
+
+    fn in_bounds(&self, bounds: &Bounds) -> bool {
+        let (mins, maxs) = (bounds[0], bounds[1]);
+        self[0] >= mins[0] - 1
+            && self[0] <= maxs[0] + 1
+            && self[1] >= mins[1] - 1
+            && self[1] <= maxs[1] + 1
+            && self[2] >= mins[2] - 1
+            && self[2] <= maxs[2] + 1
+    }
+}
+
+impl From<&(i32, i32, i32)> for Vec3 {
+    fn from(&(x, y, z): &(i32, i32, i32)) -> Self {
+        Self([x, y, z])
+    }
+}
+
+impl std::ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0[0] += rhs[0];
+        self.0[1] += rhs[1];
+        self.0[2] += rhs[2];
+    }
+}
+
+impl std::ops::Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3([self[0] + rhs[0], self[1] + rhs[1], self[2] + rhs[2]])
+    }
+}
+
+impl Vec3 {
+    const DIRECTIONS: [(i32, i32, i32); 6] = [
+        (0, 0, 1),
+        (0, 0, -1),
+        (0, 1, 0),
+        (0, -1, 0),
+        (1, 0, 0),
+        (-1, 0, 0),
+    ];
+
+    /// Returns the list of neighbors as iterator
+    pub fn neighbors(&self) -> Vec<Vec3> {
+        Self::DIRECTIONS
+            .iter()
+            .map(|&(x, y, z)| *self + Vec3::new(x, y, z))
+            .collect_vec()
+    }
+}
+
+impl Index<usize> for Vec3 {
     type Output = i32;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -26,7 +80,7 @@ impl Index<usize> for Cube {
     }
 }
 
-impl TryFrom<&str> for Cube {
+impl TryFrom<&str> for Vec3 {
     type Error = anyhow::Error;
 
     fn try_from(line: &str) -> Result<Self, Self::Error> {
@@ -43,41 +97,91 @@ impl TryFrom<&str> for Cube {
     }
 }
 
-fn part1(cubes: Vec<Cube>) -> usize {
-    let total_adjacent = cubes
-        .iter()
-        .combinations(2)
-        .filter(|cubes| cubes[0].neighbor(&cubes[1]))
-        .count();
+#[derive(Debug)]
+struct Bounds(Vec3, Vec3);
 
-    (cubes.len() * 6) - 2 * total_adjacent
+impl Bounds {
+    pub fn bounds<'a>(cubes: impl Iterator<Item = &'a Vec3>) -> Self {
+        let min = Vec3::new(i32::MAX, i32::MAX, i32::MAX);
+        let max = Vec3::new(i32::MIN, i32::MIN, i32::MIN);
+
+        let (min, max) = cubes.fold((min, max), |(mut min, mut max), cube| {
+            min.0[0] = i32::min(min[0], cube[0]);
+            min.0[1] = i32::min(min[1], cube[1]);
+            min.0[2] = i32::min(min[2], cube[2]);
+            max.0[0] = i32::max(max[0], cube[0]);
+            max.0[1] = i32::max(max[1], cube[1]);
+            max.0[2] = i32::max(max[2], cube[2]);
+            (min, max)
+        });
+        Bounds(min, max)
+    }
 }
 
-fn part2(cubes: Vec<Cube>) -> usize {
-    let total_count = part1(cubes.clone());
+impl Index<usize> for Bounds {
+    type Output = Vec3;
 
-    // find all cubes that are surrounded completely
-    let mut num_cubes = 0;
-    for cube in cubes.iter() {
-        if cubes
-            .iter()
-            .filter(|rhs| *rhs != cube && rhs.neighbor(cube))
-            .count()
-            == 6
-        {
-            num_cubes += 1;
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.0,
+            1 => &self.1,
+            _ => panic!("Index out of bounds"),
+        }
+    }
+}
+
+/// Using floodfill algorithm: https://en.wikipedia.org/wiki/Flood_fill
+fn find_exposed(cubes: &[Vec3]) -> HashSet<Vec3> {
+    let bounds = Bounds::bounds(cubes.iter());
+    let start = Vec3::default();
+
+    let mut stack = vec![start];
+    let mut visited = HashSet::new();
+    let mut exposed = HashSet::new();
+
+    visited.insert(start);
+
+    while let Some(pos) = stack.pop() {
+        for neighbor in pos.neighbors() {
+            if cubes.contains(&neighbor) || !neighbor.in_bounds(&bounds) {
+                continue;
+            }
+
+            if visited.insert(neighbor) {
+                stack.push(neighbor);
+                exposed.insert(neighbor);
+            }
         }
     }
 
-    total_count - 6 * num_cubes
+    exposed
 }
 
-fn parse(input: &str) -> Vec<Cube> {
+/// Returns the number of sides that are exposed
+fn part1(cubes: Vec<Vec3>) -> usize {
+    cubes
+        .iter()
+        .flat_map(|cube| cube.neighbors())
+        .filter(|cube| !cubes.contains(&cube))
+        .count()
+}
+
+/// Returns the number of faces exposed, excluding air pockets
+fn part2(cubes: Vec<Vec3>) -> usize {
+    let exposed_cubes = find_exposed(&cubes);
+    cubes
+        .iter()
+        .flat_map(|cube| cube.neighbors())
+        .filter(|neighbor| exposed_cubes.contains(neighbor))
+        .count()
+}
+
+fn parse(input: &str) -> Vec<Vec3> {
     input
         .lines()
         .map(str::trim)
         .filter(|line| !line.is_empty())
-        .filter_map(|line| Cube::try_from(line).ok())
+        .filter_map(|line| Vec3::try_from(line).ok())
         .collect_vec()
 }
 
