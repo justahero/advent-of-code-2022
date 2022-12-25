7e1cdedf5074adf02f37beeacb07d13c16231356
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-25 22:15:44 +0100

    Implement logic to advance all elves multiple rounds
    * add helper to get all neighbors of an elf
    * add helper method to calculate bounds of grid
    * add a few more tests to check advance & directions rotation work
    * support Grid to be printed

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,20 +1,55 @@
 //! Day 23: Unstable Diffusion
 
-use std::collections::HashSet;
+use std::{
+    cmp::{max, min},
+    collections::{HashMap, HashSet},
+    fmt::{Display, Formatter},
+};
 
 use itertools::Itertools;
 
-#[derive(Debug)]
+#[allow(dead_code)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 #[repr(u8)]
 enum Dir {
-    NW = 0,
-    N,
+    N = 0,
     NE,
     E,
     SE,
     S,
     SW,
     W,
+    NW,
+}
+
+impl Dir {
+    pub const DIRECTIONS: [Pos; 8] = [
+        Pos::new(0, -1),  // N
+        Pos::new(1, -1),  // NE
+        Pos::new(1, 0),   // E
+        Pos::new(1, 1),   // SE
+        Pos::new(0, 1),   // S
+        Pos::new(-1, 1),  // SW
+        Pos::new(-1, 0),  // W
+        Pos::new(-1, -1), // NW
+    ];
+
+    /// Returns true if this direction contains no neighbors.
+    pub fn propose(&self, neighbors: &[bool; 8]) -> bool {
+        let [n, ne, e, se, s, sw, w, nw] = neighbors;
+
+        match self {
+            Dir::N => !n && !ne && !nw,
+            Dir::S => !s && !se && !sw,
+            Dir::W => !w && !nw && !sw,
+            Dir::E => !e && !ne && !se,
+            _ => false,
+        }
+    }
+
+    pub fn direction(&self) -> Pos {
+        Self::DIRECTIONS[*self as usize]
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -24,12 +59,23 @@ struct Pos {
 }
 
 impl Pos {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub const fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
 }
 
-#[derive(Debug, Clone)]
+impl std::ops::Add for Pos {
+    type Output = Pos;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct Grid {
     elves: HashSet<Pos>,
 }
@@ -39,36 +85,106 @@ impl Grid {
         Self { elves }
     }
 
-    pub fn neighbors(&self, pos: &Pos) -> impl Iterator<Item = Pos> + '_ {
-        const neighbors: [(i32, i32); 8] = [
-            (-1, -1), // NW
-            (0, -1),  // N
-            (1, -1),  // NE
-            (1, 0),   // E
-            (1, 1),   // SE
-            (0, 1),   // S
-            (-1, 1),  // SW
-            (-1, 0),  // W
-        ];
+    /// Returns the bounds as two coordinate pairs:
+    /// upper left & lower right.
+    pub fn bounds(&self) -> (Pos, Pos) {
+        let mut minx = i32::MAX;
+        let mut miny = i32::MAX;
+        let mut maxx = i32::MIN;
+        let mut maxy = i32::MIN;
 
-        neighbors.iter().map(|&(x, y)| Pos::new(x, y))
+        for elf in self.elves.iter() {
+            minx = min(elf.x, minx);
+            miny = min(elf.y, miny);
+            maxx = max(elf.x, maxx);
+            maxy = max(elf.y, maxy);
+        }
+
+        (Pos::new(minx, miny), Pos::new(maxx, maxy))
     }
 
-    pub fn get(&self, pos: Pos) -> Option<&Pos> {
-        self.elves.get(&pos)
+    pub fn neighbors(&self, pos: Pos) -> impl Iterator<Item = Pos> + '_ {
+        Dir::DIRECTIONS.iter().cloned().map(move |dir| pos + dir)
     }
 }
 
-fn part1(grid: Grid) -> i64 {
-    for _ in 0..10 {
-        // first half of the move
-        for elf in &grid.elves {
-            let neighbors = grid.neighbors(elf).collect_vec();
+impl Display for Grid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (Pos { x: minx, y: miny }, Pos { x: maxx, y: maxy }) = self.bounds();
 
+        for y in miny..=maxy {
+            for x in minx..=maxx {
+                if self.elves.contains(&Pos::new(x, y)) {
+                    write!(f, "#")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+/// Advances all elves across the grid once!
+///
+/// ```text
+/// .....
+/// ..##.
+/// ..#..
+/// .....
+/// ..##.
+/// .....
+/// ```
+fn advance(grid: &mut Grid, directions: &[Dir]) {
+    // Maps proposed position to elves, multiple elves may propose the same position
+    let mut proposals: HashMap<Pos, Vec<Pos>> = HashMap::new();
+
+    // first phase each elf proposes a new position
+    for elf in &grid.elves {
+        let neighbors: [bool; 8] = grid
+            .neighbors(*elf)
+            .map(|neighbor| grid.elves.contains(&neighbor))
+            .collect_vec()
+            .try_into()
+            .unwrap();
+
+        // If there no elves in the adjacent fields don't move
+        if neighbors.iter().all(|occupied| !occupied) {
+            continue;
+        }
+
+        if let Some(proposed_dir) = directions.iter().find(|&dir| dir.propose(&neighbors)) {
+            proposals
+                .entry(*elf + proposed_dir.direction())
+                .or_default()
+                .push(*elf);
         }
     }
 
-    0
+    // movement phase
+    for (next_pos, elves) in proposals {
+        if elves.len() == 1 {
+            grid.elves.remove(&elves[0]);
+            grid.elves.insert(next_pos);
+        }
+    }
+}
+
+fn part1(mut grid: Grid) -> i64 {
+    let mut directions = vec![Dir::N, Dir::S, Dir::W, Dir::E];
+
+    for _ in 0..10 {
+        advance(&mut grid, &directions);
+        directions.rotate_left(1);
+    }
+
+    // determine bounds of current arrangement
+    let (min, max) = grid.bounds();
+
+    let width = (max.x - min.x) as i64 + 1;
+    let height = (max.y - min.y) as i64 + 1;
+    width * height - grid.elves.len() as i64
 }
 
 /// Parses the string, returns a map of monkey id to operation
@@ -109,6 +225,77 @@ mod tests {
         ##.#.##
         .#..#..
     ";
+
+    #[test]
+    fn check_advance() {
+        let input: &str = r#"
+            .....
+            ..##.
+            ..#..
+            .....
+            ..##.
+            .....
+        "#;
+
+        let expected: &str = r#"
+            ..##.
+            .....
+            ..#..
+            ...#.
+            ..#..
+            .....
+        "#;
+
+        let mut grid = parse(&input);
+        let directions = vec![Dir::N, Dir::S, Dir::W, Dir::E];
+        advance(&mut grid, &directions);
+        println!("{}", grid);
+
+        let expected_grid = parse(&expected);
+        assert_eq!(expected_grid, grid);
+    }
+
+    #[test]
+    fn check_advance_big() {
+        let input: &str = r#"
+            ..............
+            .......#......
+            .....#...#....
+            ...#..#.#.....
+            .......#..#...
+            ....#.#.##....
+            ..#..#.#......
+            ..#.#.#.##....
+            ..............
+            ....#..#......
+            ..............
+            ..............
+        "#;
+
+        let expected: &str = r#"
+            ..............
+            .......#......
+            ....#.....#...
+            ...#..#.#.....
+            .......#...#..
+            ...#..#.#.....
+            .#...#.#.#....
+            ..............
+            ..#.#.#.##....
+            ....#..#......
+            ..............
+            ..............
+        "#;
+
+        // simulate 2nd round
+        let directions = vec![Dir::S, Dir::W, Dir::E, Dir::N];
+
+        let mut grid = parse(&input);
+        advance(&mut grid, &directions);
+
+        let expected_grid = parse(&expected);
+        assert_eq!(expected_grid, grid);
+    }
 
     #[test]
     fn check_part1() {
