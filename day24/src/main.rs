6fac22d46d108700a3349354d984ae439b9bddfa
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-26 21:17:26 +0100

    Implement search algorithm to find shortest path.

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,10 +1,11 @@
 //! Day 24: Unstable Diffusion
 
-use itertools::Itertools;
+use std::collections::{HashMap, VecDeque};
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[repr(u8)]
 enum Direction {
-    North,
+    North = 0,
     East,
     South,
     West,
@@ -22,26 +23,37 @@ impl From<&str> for Direction {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 #[repr(u8)]
 enum Tile {
     Ground = 0,
     Wall,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Pos {
     x: i32,
     y: i32,
 }
 
 impl Pos {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub const fn new(x: i32, y: i32) -> Self {
         Self { x, y }
     }
 }
 
-#[derive(Debug)]
+impl std::ops::Add for Pos {
+    type Output = Pos;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Blizzard {
     pos: Pos,
     dir: Direction,
@@ -65,7 +77,31 @@ struct Maze {
     blizzards: Vec<Blizzard>,
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct State {
+    pos: Pos,
+    blizzards: Vec<Blizzard>,
+    time: u32,
+}
+
+impl State {
+    pub fn new(pos: Pos, blizzards: Vec<Blizzard>, time: u32) -> Self {
+        Self {
+            pos,
+            blizzards,
+            time,
+        }
+    }
+}
+
 impl Maze {
+    const DIRECTIONS: [Pos; 4] = [
+        Pos::new(0, -1),
+        Pos::new(1, 0),
+        Pos::new(0, 1),
+        Pos::new(-1, 0),
+    ];
+
     pub fn new() -> Self {
         Self {
             width: 0,
@@ -73,6 +109,70 @@ impl Maze {
             tiles: Vec::new(),
             blizzards: Vec::new(),
         }
+    }
+
+    /// Search the shortest path
+    pub fn shortest(&self) -> u32 {
+        let state = State {
+            pos: self.start(),
+            blizzards: self.blizzards.clone(),
+            time: 0,
+        };
+
+        let mut stack = VecDeque::new();
+
+        // keep all configurations of blizzards around
+        let mut cache: HashMap<u32, Vec<Blizzard>> = HashMap::new();
+
+        stack.push_back(state);
+
+        while let Some(current) = stack.pop_front() {
+            if current.pos == self.end() {
+                return current.time;
+            }
+
+            // advance all blizzards by 1 minute, check if it was calculated before
+            let next_blizzards = if let Some(blizzards) = cache.get(&(current.time + 1)) {
+                blizzards.clone()
+            } else {
+                let blizzards = self.advance(&current.blizzards);
+                cache.insert(current.time + 1, blizzards.clone());
+                blizzards
+            };
+
+            // if possible wait on the current position
+            if next_blizzards
+                .iter()
+                .find(|&blizzard| current.pos == blizzard.pos)
+                .is_none()
+            {
+                stack.push_back(State::new(
+                    current.pos,
+                    next_blizzards.clone(),
+                    current.time + 1,
+                ));
+            }
+
+            // otherwise check all directions for possible moves
+            for dir in Self::DIRECTIONS.iter() {
+                let next_pos = current.pos + *dir;
+                if self.get_tile(&next_pos) == Tile::Ground {
+                    if next_blizzards
+                        .iter()
+                        .find(|&blizzard| current.pos + *dir == blizzard.pos)
+                        .is_none()
+                    {
+                        stack.push_back(State::new(
+                            current.pos + *dir,
+                            next_blizzards.clone(),
+                            current.time + 1,
+                        ));
+                    }
+                }
+            }
+        }
+
+        0
     }
 
     pub fn add_row(mut self, mut tiles: Vec<Tile>, mut blizzards: Vec<Blizzard>) -> Self {
@@ -83,8 +183,16 @@ impl Maze {
         self
     }
 
-    pub fn get(x: i32, y: i32) -> Option<Tile> {
-        None
+    /// Advances all blizzards
+    fn advance(&self, blizzards: &[Blizzard]) -> Vec<Blizzard> {
+        Vec::new()
+    }
+
+    fn get_tile(&self, pos: &Pos) -> Tile {
+        if 0 <= pos.x && pos.x < self.width && 0 <= pos.y && pos.y < self.height {
+            return self.tiles[(pos.y * self.width + pos.x) as usize];
+        }
+        Tile::Wall
     }
 
     fn start(&self) -> Pos {
@@ -133,8 +241,8 @@ fn parse(input: &str) -> Maze {
 }
 
 fn main() {
-    let grid = parse(include_str!("input.txt"));
-    // println!("Part 1: {}", part1(grid.clone()));
+    let maze = parse(include_str!("input.txt"));
+    println!("Part 1: {}", part1(maze));
 }
 
 #[cfg(test)]
