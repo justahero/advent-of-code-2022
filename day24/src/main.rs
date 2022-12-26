0f461ee1d6e84eef1fca5b8f58927920c4ed1098
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2022-12-26 19:54:45 +0100

    Parse map & build maze

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,11 +1,6 @@
 //! Day 24: Unstable Diffusion
 
-peg::parser! {
-    grammar line_parser() for str {
-        pub(crate) rule line() -> (Vec<Tile>, Vec<Blizzard>)
-            = { (Vec::new(), Vec::new()) }
-    }
-}
+use itertools::Itertools;
 
 #[derive(Debug, PartialEq, Eq)]
 enum Direction {
@@ -15,9 +10,21 @@ enum Direction {
     West,
 }
 
+impl From<&str> for Direction {
+    fn from(v: &str) -> Self {
+        match v {
+            "^" => Direction::North,
+            ">" => Direction::East,
+            "<" => Direction::West,
+            "v" => Direction::South,
+            _ => panic!("Unknown direction found"),
+        }
+    }
+}
+
 #[derive(Debug)]
 #[repr(u8)]
-enum TileType {
+enum Tile {
     Ground = 0,
     Wall,
 }
@@ -30,7 +37,7 @@ struct Pos {
 
 impl Pos {
     pub fn new(x: i32, y: i32) -> Self {
-        Pos::new(x, y)
+        Self { x, y }
     }
 }
 
@@ -40,11 +47,10 @@ struct Blizzard {
     dir: Direction,
 }
 
-/// Describes a field with coordinates and tile types?
-#[derive(Debug)]
-struct Tile {
-    pos: Pos,
-    field: TileType,
+impl Blizzard {
+    pub fn new(pos: Pos, dir: Direction) -> Self {
+        Self { pos, dir }
+    }
 }
 
 #[derive(Debug)]
@@ -69,7 +75,7 @@ impl Maze {
         }
     }
 
-    pub fn add_row(mut self, (mut tiles, mut blizzards): (Vec<Tile>, Vec<Blizzard>)) -> Self {
+    pub fn add_row(mut self, mut tiles: Vec<Tile>, mut blizzards: Vec<Blizzard>) -> Self {
         self.height += 1;
         self.width = tiles.len() as i32;
         self.tiles.append(&mut tiles);
@@ -100,9 +106,28 @@ fn parse(input: &str) -> Maze {
         .lines()
         .map(str::trim)
         .filter(|line| !line.is_empty())
-        .fold(Maze::new(), |maze, line| {
-            maze.add_row(line_parser::line(line).unwrap())
+        .enumerate()
+        .fold(Maze::new(), |maze, (row, line)| {
+            let mut tiles = Vec::new();
+            let mut blizzards = Vec::new();
+            for (col, c) in line.chars().enumerate() {
+                let pos = Pos::new(col as i32, row as i32);
+                match c {
+                    '#' => tiles.push(Tile::Wall),
+                    _ => tiles.push(Tile::Ground),
+                }
+                match c {
+                    '>' => blizzards.push(Blizzard::new(pos, Direction::East)),
+                    '<' => blizzards.push(Blizzard::new(pos, Direction::West)),
+                    '^' => blizzards.push(Blizzard::new(pos, Direction::North)),
+                    'v' => blizzards.push(Blizzard::new(pos, Direction::South)),
+                    _ => (),
+                }
+            }
+            maze.add_row(tiles, blizzards)
         });
+
+    println!("Hello");
 
     maze
 }
